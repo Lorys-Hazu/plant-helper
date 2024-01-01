0031a6b0aad68e8731148dc0cb7df5b37dc61f80
Lorys <lorys.aveneau@gmail.com>
2024-01-01 18:32:32 +0100

    rmv dist

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { DatabaseService } from 'src/database/database.service';
-export declare class AuthService {
-    private readonly db;
-    constructor(db: DatabaseService);
-    hashPassword(password: string): Promise<string>;
-    validateUser(email: string, pass: string): Promise<any>;
-}
